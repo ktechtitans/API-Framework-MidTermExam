e1bddec71a28afe86407db5a5e8a6ccb9d956a28
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-10-18 17:06:49 -0400

    Connected MongoDB.

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ const app = express();
 //MongoDb Atlas connection string
 const mongoURI = 'mongodb+srv://kanchan:<db_password>@cluster0.t0y4q.mongodb.net/';
 
-
+// Connect to MongoDB
+mongoose
+  .connect(MongodbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("You are connected to MongoDB"))
+  .catch((err) => console.error(err));
+  
 // Start the server
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
