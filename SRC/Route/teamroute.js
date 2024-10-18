d473bf835a411a8a73ff4baaf6f9601183f006e8
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-10-18 15:22:53 -0400

    Add routes for get all teams.

--- SRC/Route/teamroute.js
+++ SRC/Route/teamroute.js
@@ -1,3 +1,7 @@
 // Import the express module to create a router
 const express = require("express");
 const router = express.Router();
+
+// Route to get all teams
+router.get("/teams", getAllTeams);
+
