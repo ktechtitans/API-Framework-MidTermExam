bf902dbacbe0571f88aca1ec7d574a67214a05d0
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-10-18 17:05:21 -0400

    Connected mongoURI in server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 //Initialize the express app
 const app = express();
 
+//MongoDb Atlas connection string
+const mongoURI = 'mongodb+srv://kanchan:<db_password>@cluster0.t0y4q.mongodb.net/';
+
+
 // Start the server
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
