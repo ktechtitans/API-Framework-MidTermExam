f308a39b76a0f3f8ad0cd37a9275e70a0e0944ee
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-10-18 14:52:22 -0400

    Created team schema in model.js file.

--- SRC/Model/teammodel.js
+++ SRC/Model/teammodel.js
@@ -1,3 +1,14 @@
 // Import Mongoose library
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+// Define the Team schema with appropriate fields
+const TeamSchema = new Schema({
+  teamId: { type: Number, required: true, unique: true },
+  teamData: {
+    teamName: { type: String, required: true },
+    city: { type: String, required: true },
+    founded: { type: String, required: true },
+    coach: { type: String, required: true },
+  },
+});
