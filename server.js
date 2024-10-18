e013eb7ef20060c1272e8a5c5ac106dcc9a6e8f6
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-10-18 14:29:19 -0400

    Created a Server

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 // Import libraries and modules
 const express = require('express');
 const mongoose = require('mongoose');
+
+//Initialize the express app
+const app = express();
+
+// Start the server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
